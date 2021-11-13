c35d72d7380534199209fc54f15e573e7d1024cf
ivan-boginski <aisivan36@gmail.com>
2021-11-13 21:48:19 +0700

    Register page

---
+++ src/pages/Register.jsx
@@ -0,0 +1,84 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+	width: 100vw;
+	height: 100vh;
+	background: linear-gradient(
+			rgba(255, 255, 255, 0.5),
+			rgba(255, 255, 255, 0.5)
+		),
+		url("https://cdn.pling.com/img/e/3/b/a/197d5ba9be46a9bfaf3e2a8f092738e4c8f2058e06c670c148305101d140e1719a2d.jpg")
+			center;
+	background-size: cover;
+
+	display: flex;
+	align-items: center;
+	justify-content: center;
+`;
+
+const Wrapper = styled.div`
+	padding: 20px;
+	width: 40%;
+	background-color: white;
+`;
+
+const Title = styled.h1`
+	font-size: 24px;
+	font-weight: 300;
+`;
+
+const Form = styled.form`
+	display: flex;
+	flex-wrap: wrap;
+`;
+
+const Input = styled.input`
+	flex: 1;
+	min-width: 40%;
+	margin: 20px 10px 0px 0px;
+	padding: 10px;
+`;
+
+const Agreement = styled.span`
+	font-size: 12px;
+	margin: 20px 0px;
+`;
+
+const Button = styled.button`
+	width: 40%;
+	border: none;
+	padding: 15px 20px;
+	background-color: teal;
+	color: white;
+	cursor: pointer;
+
+	&:hover {
+		color: #fff8f8;
+		background-color: #0a6e6e;
+	}
+`;
+
+const Register = () => {
+	return (
+		<Container>
+			<Wrapper>
+				<Title>CREATE AN ACCOUNT</Title>
+				<Form>
+					<Input placeholder="name" />
+					<Input placeholder="last name" />
+					<Input placeholder="username" />
+					<Input placeholder="email" />
+					<Input placeholder="password" />
+					<Input placeholder="confirm password" />
+					<Agreement>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. A, iusto
+						natus! Placeat, <b>inventore?</b>
+					</Agreement>
+					<Button>CREATE</Button>
+				</Form>
+			</Wrapper>
+		</Container>
+	);
+};
+
+export default Register;
